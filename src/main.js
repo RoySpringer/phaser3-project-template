846c525a494bb3d968e8b08f817f1906ffaaba76
Roy Springer <rspringer@slip-app.nl>
2022-09-29 23:15:15 +0200

    Added ESLint and a Prettier config

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
-import "./style.css";
-import Phaser from "phaser";
+import './style.css';
+import Phaser from 'phaser';
 
 class MyGame extends Phaser.Scene {
   constructor() {
-    super("game");
+    super('game');
   }
 
   preload() {
-    this.load.image("logo", "/assets/logo.png");
+    this.load.image('logo', '/assets/logo.png');
   }
 
   create() {
-    const logo = this.add.image(400, 150, "logo");
+    const logo = this.add.image(400, 150, 'logo');
 
     this.tweens.add({
       targets: logo,
       y: 450,
       duration: 2000,
-      ease: "Power2",
+      ease: 'Power2',
       yoyo: true,
       loop: -1,
     });
@@ -26,7 +26,7 @@ class MyGame extends Phaser.Scene {
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-game-container",
+  parent: 'phaser-game-container',
   width: 800,
   height: 450,
   scene: MyGame,
